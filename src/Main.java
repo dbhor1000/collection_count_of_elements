6567c1d0cf4941fd97d7a1722f051f8002cd7a33
dbhor <dbhor1000@gmail.com>
2024-06-30 21:05:32 +0300

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,10 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
 
-
         //Практическое задание - Collection - count of elements
         //Напишите метод, который получает на вход массив элементов и возвращает Map ключи в котором - элементы, а значения - сколько раз встретился этот элемент
 
-
-
         Object[] arr1 = {"Alpha", "Bravo", "Bravo", "Charlie", "Delta", "Echo", 1, 2, ((char) 6000), ((char) 6000), ((char) 6000)};
-
-
 
         Map<Object,Long> map = Arrays.stream(arr1)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
