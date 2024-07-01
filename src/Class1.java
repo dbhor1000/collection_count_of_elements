ae941e42da251e22bc5068c823d7f8b83bb87d43
dbhor <dbhor1000@gmail.com>
2024-07-01 10:49:18 +0300

    Commit 3

---
+++ src/Class1.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Class1 {
+
+    public static Map<Object, Long> countOfElements(Object[] arr1){
+        Map<Object,Long> map = Arrays.stream(arr1)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(map);
+        return map;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,12 @@
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
 
         //Практическое задание - Collection - count of elements
         //Напишите метод, который получает на вход массив элементов и возвращает Map ключи в котором - элементы, а значения - сколько раз встретился этот элемент
-
         Object[] arr1 = {"Alpha", "Bravo", "Bravo", "Charlie", "Delta", "Echo", 1, 2, ((char) 6000), ((char) 6000), ((char) 6000)};
 
-        Map<Object,Long> map = Arrays.stream(arr1)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        System.out.println(map);
-        //Output: {ᝰ=3, 1=1, 2=1, Echo=1, Delta=1, Charlie=1, Alpha=1, Bravo=2}
+        Class1.countOfElements(arr1);
 
+        //Output: {ᝰ=3, 1=1, 2=1, Echo=1, Delta=1, Charlie=1, Alpha=1, Bravo=2}
     }
 }
